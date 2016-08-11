6be0da7ab1240dd2b5a78648e622a235b8075d67
sedrick.one@gmail.com <sedrick.one@gmail.com>
2016-08-11 10:36:42 +0600

    Создаю серверную часть программы

---
+++ src/MainClass.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * Created by ОБИ on 11.08.2016.
+ */
+public class MainClass {
+    public static void main(String[] args) throws IOException {
+        ServerSocket socket=new ServerSocket(12345);
+        File file=new File()
+    }
+}
